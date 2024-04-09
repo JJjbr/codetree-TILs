25cf3c634f1fb3bc6d69831905376dd57ed1e9ae
boreum Jeon <84780381+JJjbr@users.noreply.github.com>
2024-04-10 00:48:57 +0900

    Title: 왕실의 기사 대결, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240410/왕실의 기사 대결/royal-knight-duel.cpp
+++ 240410/왕실의 기사 대결/royal-knight-duel.cpp
@@ -1,4 +1,6 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 struct Knight {
@@ -8,41 +10,33 @@ struct Knight {
 int L, N, Q;
 int map[42][42];
 int knight_map[42][42];
+int tmp_knight_map[42][42];
 Knight knights[32];
 int dy[4] = { -1, 0, 1, 0 };
 int dx[4] = { 0, 1, 0, -1 };
 
 void updateKnight(int id, int dir) {
+	bool visit[32] = { 0, };
+	for (int y = 1; y <= L; y++) {
+		for (int x = 1; x <= L; x++) {
+			int now = knight_map[y][x];
+			if (now == 0 || visit[now]) continue;
+			visit[now] = true;
+
+			if (knights[now].k <= 0) continue;
+			if (knights[now].r != y || knights[now].c != x) knights[now].move = true;
+			knights[now].r = y;
+			knights[now].c = x;
+		}
+	}
+
 	for (int i = 1; i <= N; i++) {
 		if (!knights[i].move) continue;
 		knights[i].move = 0;
 
+		if (i == id || knights[i].k <= 0) continue;
 		Knight now = knights[i];
-		int ny = now.r + dy[dir];
-		int nx = now.c + dx[dir];
 
-		//for (int y = now.r; y < now.r + now.h; y++) {
-		//	for (int x = now.c; x < now.c + now.w; x++) {
-		//		if (knight_map[y][x] == i) {
-		//			knight_map[y][x] = 0;
-		//		}
-		//	}
-		//}
-		//for (int y = ny; y < ny + now.h; y++) {
-		//	for (int x = nx; x < nx + now.w; x++) {
-		//		if (knight_map[y][x] == 0) {
-		//			knight_map[y][x] = i;
-		//		}
-		//	}
-		//}
-
-		if (now.k <= 0) continue;
-		knights[i].r = ny;
-		knights[i].c = nx;
-
-		if (i == id) continue;
-		now = knights[i];
-		
 		int cnt = 0;
 		for (int y = now.r; y < now.r + now.h; y++) {
 			for (int x = now.c; x < now.c + now.w; x++) {
@@ -72,11 +66,7 @@ bool moveKnight(int id, int dir) {
 	for (int y = ny; y < ny + now.h; y++) {
 		for (int x = nx; x < nx + now.w; x++) {
 			if (map[y][x] == 2) return false;
-		}
-	}
 
-	for (int y = ny; y < ny + now.h; y++) {
-		for (int x = nx; x < nx + now.w; x++) {
 			if (knight_map[y][x] > 0 && knight_map[y][x] != id) {
 				if (!moveKnight(knight_map[y][x], dir)) return false;
 			}
@@ -85,24 +75,25 @@ bool moveKnight(int id, int dir) {
 
 	for (int y = now.r; y < now.r + now.h; y++) {
 		for (int x = now.c; x < now.c + now.w; x++) {
-			if (knight_map[y][x] == id) {
-				knight_map[y][x] = 0;
+			if (tmp_knight_map[y][x] == id) {
+				tmp_knight_map[y][x] = 0;
 			}
 		}
 	}
 	for (int y = ny; y < ny + now.h; y++) {
 		for (int x = nx; x < nx + now.w; x++) {
-			if (knight_map[y][x] == 0) {
-				knight_map[y][x] = id;
+			if (tmp_knight_map[y][x] == 0) {
+				tmp_knight_map[y][x] = id;
 			}
 		}
 	}
-	knights[id].move = 1;
 	return true;
 }
 
 int main() {
 	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	//freopen("input.txt", "r", stdin);
+
 	cin >> L >> N >> Q;
 	for (int y = 1; y <= L; y++) {
 		for (int x = 1; x <= L; x++) {
@@ -124,7 +115,9 @@ int main() {
 	for (int i = 1; i <= Q; i++) {
 		cin >> id >> dir;
 		if (knights[id].k <= 0) continue;
+		memcpy(tmp_knight_map, knight_map, sizeof(knight_map));
 		if (!moveKnight(id, dir)) continue;
+		memcpy(knight_map, tmp_knight_map, sizeof(tmp_knight_map));
 		updateKnight(id, dir);
 	}
 
