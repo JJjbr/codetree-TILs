9cfbb68580d09355c98d546e78855dfb641eefe2
boreum Jeon <84780381+JJjbr@users.noreply.github.com>
2024-04-11 02:47:12 +0900

    Title: 메이즈 러너, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240411/메이즈 러너/maze-runner.cpp
+++ 240411/메이즈 러너/maze-runner.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstring>
-#include <queue>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 struct People {
@@ -11,151 +12,87 @@ struct Coord {
 	int y, x;
 };
 
-struct Check {
-	int p, y, x;
-	bool operator<(Check right) const {
-		if (y > right.y) return true;
-		else if(y == right.y) {
-			if (x > right.x) return true;
-			else if (x == right.x) {
-				if (p > right.p) return true;
-			}
-		}
-		return false;
-	}
-};
-
 struct Minarea {
-	int sy, sx, ey, ex;
+	int y, x, length;
 };
 
 int N, M, K;
 int map[12][12];
-//int people_map[12][12];
 People people[12];
 Coord out;
-int min_area = 1e6;
-Minarea min_area_coord;
+vector<Minarea> min_area;
 int ans;
+int out_people;
 int dy[4] = { -1, 1, 0, 0 };
 int dx[4] = { 0, 0, -1, 1 };
 
-void rotateMaze() {
-	int sy = min_area_coord.sy, sx = min_area_coord.sx, ey = min_area_coord.ey, ex = min_area_coord.ex;
-	//cout << sy << " " << sx << " " << ey << " " << ex << "\n";
+bool cmp(Minarea left, Minarea right) {
+	if (left.length == right.length) {
+		if(left.y == right.y)
+			return left.x < right.x;
+		return left.y < right.y;
+	}
+	return left.length < right.length;
+}
+
+void rotateMaze(int sy, int sx, int len) {
 	int tmp_map[12][12] = { 0, };
 	memcpy(tmp_map, map, sizeof(map));
-	int sum = sy + ex;
-	int sub = sy - sx;
-	priority_queue<Check> pq;
-	for (int i = 1; i <= M; i++) {
-		if (people[i].y >= sy && people[i].x >= sx && people[i].y <= ey && people[i].x <= ex) {
-			pq.push({ i, people[i].y, people[i].x });
-		}
-	}
-	Check now = pq.top();
-	pq.pop();
 	bool change_out = false;
-	for (int y = sy; y <= ey; y++) {
-		for (int x = sx; x <= ex; x++) {
-			tmp_map[sub + x][sum - y] = map[y][x];
-			if (map[y][x] > 0) {
-				tmp_map[sub + x][sum - y] = map[y][x] - 1;
+	for (int y = 0; y <= len; y++) {
+		for (int x = 0; x <= len; x++) {
+			int py = sy + y;
+			int px = sx + x;
+
+			int ny = sy + x;
+			int nx = sx + len - y;
+
+			if (map[py][px] == 0) {
+				tmp_map[ny][nx] = 0;
 			}
-			if (!change_out && y == out.y && x == out.x) {
-				out.y = sub + x;
-				out.x = sum - y;
+			else if (map[py][px] > 0) {
+				tmp_map[ny][nx] = map[py][px] - 1;
+			}
+
+			if (!change_out && py == out.y && px == out.x) {
+				out.y = ny;
+				out.x = nx;
 				change_out = true;
-				//cout << "good " << y << " " << x << " " << out.y << " " << out.x << "\n";
-			}
-			while (now.y == y && now.x == x) {
-				people[now.p].y = sub + x;
-				people[now.p].x = sum - y;
-				//people[now.p].dist = abs(people[now.p].y - out.y) + abs(people[now.p].x - out.x);
-				if (pq.empty()) break;
-				now = pq.top();
-				pq.pop();
 			}
 		}
 	}
 	memcpy(map, tmp_map, sizeof(tmp_map));
-}
 
-void getMinArea(int ny, int nx) {
-	int y = ny - out.y;
-	int x = nx - out.x;
-	//cout << ny << " " << nx << " " << out.y << " " << out.x << "\n";
-	
-	int length = 0, area = 0;
-	Minarea tmp = { out.y, out.x, out.y, out.x };
-	if (abs(x) < abs(y)) {
-		length = y;
-		if (length < 0) { // 상
-			int start = out.x < nx ? out.x : nx;
-			int end = out.x > nx ? out.x : nx;
-			for (int c = end + length; c <= start; c++) {
-				if (c <= 0 || c > N || c + abs(length) <= 0 || c + abs(length) > N) continue;
-				tmp.sy = ny;
-				tmp.sx = c;
-				tmp.ey = out.y;
-				tmp.ex = c + abs(length);
-				//cout << "up\n";
-				break;
-			}
-		}
-		else if (length > 0) { // 하
-			int start = out.x < nx ? out.x : nx;
-			int end = out.x > nx ? out.x : nx;
-			for (int c = end - length; c <= start; c++) {
-				if (c <= 0 || c > N || c + abs(length) <= 0 || c + abs(length) > N) continue;
-				tmp.sy = out.y;
-				tmp.sx = c;
-				tmp.ey = ny;
-				tmp.ex = c + abs(length);
-				//cout << "down\n";
-				break;
-			}
-		}
-	}
-	else if (abs(x) > abs(y)) {
-		length = x;
-		if (length < 0) { // 좌
-			int start = out.y < ny ? out.y : ny;
-			int end = out.y > ny ? out.y : ny;
-			for (int r = end + length; r <= start; r++) {
-				if (r <= 0 || r > N || r + abs(length) <= 0 || r + abs(length) > N) continue;
-				tmp.sy = r;
-				tmp.sx = nx;
-				tmp.ey = r + abs(length);
-				tmp.ex = out.x;
-				//cout << "left\n";
-				break;
-			}
-		}
-		else if (length > 0) { // 우
-			int start = out.y < ny ? out.y : ny;
-			int end = out.y > ny ? out.y : ny;
-			for (int r = end - length; r <= start; r++) {
-				if (r <= 0 || r > N || r + abs(length) <= 0 || r + abs(length) > N) continue;
-				tmp.sy = r;
-				tmp.sx = out.x;
-				tmp.ey = r + abs(length);
-				tmp.ex = nx;
-				//cout << "right\n";
-				break;
-			}
-		}
-	}
-	//cout << tmp.sy << " " << tmp.sx << " " << tmp.ey << " " << tmp.ex << "\n";
+	for (int i = 1; i <= M; i++) {
+		if (people[i].dist == 0) continue;
+		if (people[i].y >= sy && people[i].y <= sy + len && people[i].x >= sx && people[i].x <= sx + len) {
+			int y = people[i].y - sy;
+			int x = people[i].x - sx;
 
-	area = (abs(length) + 1) * (abs(length) + 1);
-	if (area < min_area) {
-		min_area = area;
-		min_area_coord = tmp;
+			int ny = sy + x;
+			int nx = sx + len - y;
+
+			people[i].y = ny;
+			people[i].x = nx;
+		}
 	}
 }
 
-bool movePerson(int p) {
+void getMinArea(int y, int x) {
+	int length_y = abs(y - out.y);
+	int length_x = abs(x - out.x);
+	int length = max(length_y, length_x);
+
+	int sy = max(y, out.y) - length;
+	if (sy < 1) sy = 1;
+
+	int sx = max(x, out.x) - length;
+	if (sx < 1) sx = 1;
+
+	min_area.push_back({ sy, sx, length });
+}
+
+void movePerson(int p) {
 	People now = people[p];
 	for (int i = 0; i < 4; i++) {
 		int ny = now.y + dy[i];
@@ -163,27 +100,26 @@ bool movePerson(int p) {
 		if (ny <= 0 || ny > N || nx <= 0 || nx > N) continue;
 		if (map[ny][nx] > 0) continue;
 		int dist = abs(ny - out.y) + abs(nx - out.x);
-		if (dist < now.dist) {
-			people[p].y = ny;
-			people[p].x = nx;
-			people[p].dist = dist;
-			return true;
-		}
+		if (dist >= now.dist) continue;
+
+		ans++;
+		people[p].y = ny;
+		people[p].x = nx;
+		people[p].dist = dist;
+		if (dist == 0) out_people++;
+		break;
 	}
-	return false;
 }
 
 void movePeople() {
 	for (int i = 1; i <= M; i++) {
 		if (people[i].dist == 0) continue;
-		if (movePerson(i)) {
-			ans++;
-		}
+		movePerson(i);
 	}
 }
 
 int main() {
-	// 입력
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 	cin >> N >> M >> K;
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
@@ -192,58 +128,27 @@ int main() {
 	}
 	for (int i = 1; i <= M; i++) {
 		cin >> people[i].y >> people[i].x;
-		people[i].dist = -1;
+		people[i].dist = 1e6;
 	}
 	cin >> out.y >> out.x;
 
-	// 초기 거리 구하기
-	//for (int i = 1; i <= M; i++) {
-	//	people[i].dist = abs(people[i].y - out.y) + abs(people[i].x - out.x);
-	//}
-
-	//cout << ans << "\n";
-	//for (int i = 1; i <= N; i++) {
-	//	for (int j = 1; j <= N; j++) {
-	//		cout << map[i][j] << " ";
-	//	}
-	//	cout << "\n";
-	//}
-
 	for (int i = 1; i <= K; i++) {
-		for (int i = 1; i <= M; i++) {
-			if (people[i].dist == 0) continue;
-			people[i].dist = abs(people[i].y - out.y) + abs(people[i].x - out.x);
-		}
-
-		//cout << "Time " << i << "\n";
-		movePeople();
-		//for (int j = 1; j <= M; j++) {
-		//	if (people[j].dist == 0) continue;
-		//	cout << people[j].y << " " << people[j].x << "\n";
-		//}
-
-		min_area = 1e6;
 		for (int j = 1; j <= M; j++) {
 			if (people[j].dist == 0) continue;
-			//cout << people[j].y << " " << people[j].x << "\n";
+			people[j].dist = abs(people[j].y - out.y) + abs(people[j].x - out.x);
+		}
+		movePeople();
+
+		if (out_people >= M) break;
+
+		for (int j = 1; j <= M; j++) {
+			if (people[j].dist == 0) continue;
 			getMinArea(people[j].y, people[j].x);
 		}
 
-		rotateMaze();
-
-		//cout << "ok " << min_area << "\n";
-		//for (int i = 1; i <= N; i++) {
-		//	for (int j = 1; j <= N; j++) {
-		//		cout << map[i][j] << " ";
-		//	}
-		//	cout << "\n";
-		//}
-		//for (int j = 1; j <= M; j++) {
-		//	if (people[j].dist == 0) continue;
-		//	cout << people[j].y << " " << people[j].x << "\n";
-		//}
-		//cout << ans << "\n";
-		//cout << out.y << " " << out.x << "\n";
+		sort(min_area.begin(), min_area.end(), cmp);
+		rotateMaze(min_area[0].y, min_area[0].x, min_area[0].length);
+		min_area.clear();
 	}
 	cout << ans << "\n";
 	cout << out.y << " " << out.x << "\n";
